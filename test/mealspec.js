81b6bdedaf2c1acc0af1c86a8de2522966d675b3
weaverb <bryanweaver@outlook.com>
2015-07-23 20:43:12 -0500

    initial commit

---
+++ test/mealspec.js
@@ -0,0 +1,11 @@
+/// <reference path="../typings/mocha/mocha.d.ts"/>
+
+
+var should = require('should');
+var supertest = require('supertest');
+
+describe('meals', function(){
+	it('pass', function(done){
+		done();
+	});
+});
